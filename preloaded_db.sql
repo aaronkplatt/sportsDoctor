fcb4b1e1e4b6aad1fff0338cdb4f30de6b6bead2
Aaron Platt <aaron.k.platt@gmail.com>
2021-04-07 14:37:47 -0700

    i got the show all patient console table to function

--- preloaded_db.sql
+++ preloaded_db.sql
@@ -2,10 +2,50 @@ DROP DATABASE IF EXISTS sports_doctor_db;
 CREATE DATABASE sports_doctor_db;
 USE sports_doctor_db;
 
-CREATE TABLE ...(
-  
+--patients table needs fk set up--
+CREATE TABLE patient(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  doctor_id INTEGER(11) NOT NULL,
+  room_number INTEGER(11) NOT NULL,
+  pain_id INTEGER(11) NOT NULL,
+  injury_location VARCHAR(30) NOT NULL,
+  injury_id INTEGER(11) NOT NULL,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE ...(
-  
+--doctor needs fk set up--
+CREATE TABLE doctor(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  department_id INTEGER(11) NOT NULL,
+  PRIMARY KEY (id)
 );
+
+CREATE TABLE pain(
+    id INTEGER(11) NOT NULL,
+    pain_level VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE injury(
+    id INTEGER(11) NOT NULL,
+    injury_name VARCHAR(30) NOT NULL,
+    department_id INTEGER NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE department(
+    id INTEGER(11) NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    injury_id INTEGER NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- INSERT INTO `sports_doctor_db`.`injury` (`id`, `injury_name`, `department_id`) VALUES ('1', 'Sprain', '2');
+-- INSERT INTO `sports_doctor_db`.`injury` (`id`, `injury_name`, `department_id`) VALUES ('2', 'Strain', '5');
+-- INSERT INTO `sports_doctor_db`.`injury` (`id`, `injury_name`, `department_id`) VALUES ('3', 'Fracture', '3');
+-- INSERT INTO `sports_doctor_db`.`injury` (`id`, `injury_name`, `department_id`) VALUES ('4', 'Concussion', '4');
+-- INSERT INTO `sports_doctor_db`.`injury` (`id`, `injury_name`, `department_id`) VALUES ('5', 'Overuse', '1');
--- server.js
+++ server.js
@@ -28,3 +28,49 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
 });
+
+//THE FIRST QUESTION PROMPTED
+initialQuestion();
+function initialQuestion() {
+    inquirer
+    .prompt([
+        //WHAT WOULD YOU LIKE TO DO? (initialQuestion)
+        {
+            type: "list",
+            name: "initialQuestion",
+            message: "Welcome to the Sports Doctor! How can I help you today?",
+            choices: ["Add a new Patient", "Remove a Patient", "View all Patients"]
+        }
+    ])
+    .then(function(answers) {
+        const initialQuestion = answers.initialQuestion;
+        if (initialQuestion === "Add a new Patient") {
+            // addNewPatient(); // to do
+            console.log("it worked!")
+        }
+        else if (initialQuestion === "Remove a Patient") {
+            removePatient(); // to do
+        }
+        else if (initialQuestion === "View all Patients") {
+            viewAllPatients(); // DONE
+        }
+    });
+}
+                                                                                       
+//VIEW ALL PATIENTS (TO DO)
+function viewAllPatients() {
+    connection.query(
+        `SELECT p.id, concat(p.first_name, ' ', p.last_name) AS Patient, concat(d.first_name, ' ', d.last_name) AS Doctor, h.pain_level, p.injury_location, i.injury_name, p.room_number
+        FROM sports_doctor_db.patient AS p 
+        JOIN sports_doctor_db.doctor AS d ON p.doctor_id = d.id
+        JOIN sports_doctor_db.pain AS h ON p.pain_id = h.id
+        JOIN sports_doctor_db.injury AS i on p.injury_id = i.id;`,
+            function(err, res) {
+                if (err) throw err;
+                console.log("\n");
+            console.table(res);
+            //go back to inital
+            initialQuestion();
+    });
+}
+
